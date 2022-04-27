abc796d230f6412cff77fd4edeae0b440e4d6323
kakandaville <maksimantonov1994@yandex.ru>
2022-04-27 13:37:23 +0300

    Little hihi

--- hihihih.py
+++ hihihih.py
@@ -6,3 +6,22 @@ def plus(a,b):
 
 
 print(plus(10,10))
+
+from math import sqrt
+
+
+def solve(a, b, c):
+    d = sqrt(b ** 2 - 4 * a * c)
+
+    x1 = (-b - d) / (2 * a)
+    x2 = (-b + d) / (2 * a)
+
+    return x1, x2
+
+
+# считываем данные
+a, b, c = int(input()), int(input()), int(input())
+
+# вызываем функцию
+x1, x2 = solve(a, b, c)
+print(x1, x2)
